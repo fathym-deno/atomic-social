6ebed76e3bc8a130dbab31d610f2d5b5664d91f6
michael.gearhardt@fathym.com <michael.gearhardt@fathym.com>
2023-08-22 12:30:49 +0000

    "Created FeedCardList component in src/organisms directory"

---
+++ src/organisms/FeedCardList.tsx
@@ -0,0 +1,27 @@
+import { ComponentChildren, JSX } from "preact";
+import { Action, ActionGroup, ActionProps, classSet } from "../src.deps.ts";
+import { FeedCard, FeedCardProps } from "./FeedCard.tsx";
+
+export interface FeedCardListProps extends JSX.HTMLAttributes<HTMLDivElement> {
+  cards: FeedCardProps[];
+  loadMore?: ActionProps | ComponentChildren;
+}
+
+export function FeedCardList(props: FeedCardListProps): JSX.Element {
+  const { cards, loadMore, ...rest } = props;
+
+  return (
+    <div {...rest}>
+      {cards.map((card, index) => (
+        <FeedCard key={index} {...card} />
+      ))}
+      {loadMore && (
+        <div class="flex justify-center mt-4">
+          <ActionGroup>
+            <Action {...loadMore} />
+          </ActionGroup>
+        </div>
+      )}
+    </div>
+  );
+}
