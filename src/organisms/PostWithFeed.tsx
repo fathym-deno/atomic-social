a728802f0bd042e8cf62cccd7d71311ebfcf78f8
michael.gearhardt@fathym.com <michael.gearhardt@fathym.com>
2023-08-22 12:36:34 +0000

    "Refactor PostWithFeed to use FeedCardList instead of FeedCard"

--- src/organisms/PostWithFeed.tsx
+++ src/organisms/PostWithFeed.tsx
@@ -1,19 +1,19 @@
 import { ComponentChildren, JSX } from "preact";
-import { FeedCard, FeedCardProps } from "./FeedCard.tsx";
+import { FeedCardList, FeedCardListProps } from "./FeedCardList";
 import { PostForm, PostFormProps } from "./PostForm.tsx";
 
 export interface PostWithFeedProps extends JSX.HTMLAttributes<HTMLDivElement> {
-  feedCardProps: FeedCardProps;
+  feedCardListProps: FeedCardListProps;
   postFormProps: PostFormProps;
 }
 
 export function PostWithFeed(props: PostWithFeedProps): JSX.Element {
-  const { feedCardProps, postFormProps, ...divProps } = props;
+  const { feedCardListProps, postFormProps, ...divProps } = props;
 
   return (
-    <div {...divProps}>
+    <div {...divProps} class={classSet("flex", "flex-col")}>
       <PostForm {...postFormProps} />
-      <FeedCard {...feedCardProps} />
+      <FeedCardList {...feedCardListProps} />
     </div>
   );
 }
