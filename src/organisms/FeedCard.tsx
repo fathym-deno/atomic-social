0c6c00170ada548dcad438f035f681e92bc8f401
mcgear <michael.gearhardt@gmail.com>
2023-08-22 22:54:04 -0600

    Bring back FeedCard

---
+++ src/organisms/FeedCard.tsx
@@ -0,0 +1,52 @@
+import { ComponentChildren, JSX } from "preact";
+import { Action, ActionGroup, ActionProps, classSet } from "../src.deps.ts";
+
+export interface FeedCardProps extends JSX.HTMLAttributes<HTMLDivElement> {
+  username: string;
+  avatar: string;
+  timestamp: string;
+  children: ComponentChildren;
+  image?: string;
+  video?: string;
+  actions?: ActionProps[] | ComponentChildren;
+}
+
+export function FeedCard(props: FeedCardProps): JSX.Element {
+  const actions = Array.isArray(props.actions) ? props.actions : undefined;
+  return (
+    <div class={classSet(props, "bg-white rounded-lg shadow-md p-4")}>
+      <div class="flex items-center">
+        <img
+          src={props.avatar}
+          class="w-10 h-10 rounded-full mr-2"
+          alt={props.username}
+        />
+        <div class="flex flex-col">
+          <span class="font-bold">{props.username}</span>
+          <span class="text-gray-500 text-sm">{props.timestamp}</span>
+        </div>
+      </div>
+      {props.children}
+      {props.image && (
+        <img src={props.image} class="mt-4 rounded-lg" alt="Post Image" />
+      )}
+      {props.video && (
+        <video src={props.video} class="mt-4 rounded-lg" controls></video>
+      )}
+      <div class="flex justify-center align-middle mt-4">
+        <ActionGroup class="flex-grow flex justify-center align-middle">
+          <>
+            {!actions && props.actions}
+            {actions &&
+              actions.map((action, index) => (
+                <Action
+                  key={index}
+                  {...action}
+                />
+              ))}
+          </>
+        </ActionGroup>
+      </div>
+    </div>
+  );
+}
