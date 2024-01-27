a5fbb840944dca171b8a521b81af569d995486b5
mcgear <michael.gearhardt@gmail.com>
2024-01-26 21:36:02 -0700

    fix -:

--- src/src.deps.ts
+++ src/src.deps.ts
@@ -3,4 +3,4 @@ export { useState } from "preact/hooks";
 
 export * from "https://deno.land/x/fathym_common@v0.0.137/mod.ts";
 
-export * from "https://deno.land/x/fathym_atomic@v0.0.117/mod.ts";
+export * from "https://deno.land/x/fathym_atomic@v0.0.118/mod.ts";
