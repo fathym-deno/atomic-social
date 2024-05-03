182daa8b7d04782ea385c7ee9a12b21f3db7226d
mcgear <michael.gearhardt@gmail.com>
2024-05-03 09:14:52 -0600

    ref updates

--- src/src.deps.ts
+++ src/src.deps.ts
@@ -4,6 +4,6 @@ export {
 } from "https://esm.sh/preact@10.20.1";
 export { useState } from "https://esm.sh/preact@10.20.1/hooks";
 
-export * from "https://deno.land/x/fathym_common@v0.0.177/mod.ts";
+export * from "https://deno.land/x/fathym_common@v0.0.183/mod.ts";
 
-export * from "https://deno.land/x/fathym_atomic@v0.0.139/mod.ts";
+export * from "https://deno.land/x/fathym_atomic@v0.0.148/mod.ts";
